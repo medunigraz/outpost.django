dc9d79d2accb06d536d0c6e3a5bba8d5fda9193e
Michael Fladischer <michael@fladi.at>
2023-02-23 13:37:03 +0100

    Set up ASGI autodetection.

--- src/outpost/django/asgi.py
+++ src/outpost/django/asgi.py
@@ -1,15 +1,45 @@
 import os
+import logging
 import django
+from importlib import import_module
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outpost.django.settings")
-django.setup()  # NOQA
 
+django.setup()
+
+from asgiref.wsgi import WsgiToAsgi
+from django.apps import apps
+#from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 
-from .base.routing import websocket_urlpatterns
 
+
+logger = logging.getLogger(__name__)
+
+#django_asgi_app = get_asgi_application()
+django_asgi_app = WsgiToAsgi(get_wsgi_application())
+
+urls = list()
+worker = dict()
+
+for app in sorted(apps.get_app_configs(), key=lambda app: app.label):
+    try:
+        logger.debug(f"Looking for channels in {app.name}")
+        module = import_module(f"{app.name}.channels")
+        urls.extend(getattr(module, "urls", []))
+        worker.update({k: v for k, v in getattr(module, "worker", {}).items()})
+    except ModuleNotFoundError:
+        pass
+    except Exception:
+        logger.warn(f"Failed to import channels from {app.name}")
+        pass
 
 application = ProtocolTypeRouter(
-    {"websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))}
+    {
+        "websocket": URLRouter(urls),
+        "channel": ChannelNameRouter(worker),
+        "http": django_asgi_app,
+    }
 )
