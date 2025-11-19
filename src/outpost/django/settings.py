451037712d9d25aeaddb07707d2fef84b79ab632
Michael Fladischer <michael.fladischer@medunigraz.at>
2025-11-19 11:14:06 +0100

    Remove deprecated corsheaders.middleware.CorsPostCsrfMiddleware.

--- src/outpost/django/settings.py
+++ src/outpost/django/settings.py
@@ -93,7 +93,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "corsheaders.middleware.CorsPostCsrfMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "oauth2_provider.middleware.OAuth2TokenMiddleware",
     "djangosaml2.middleware.SamlSessionMiddleware",
